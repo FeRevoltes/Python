953be2d202a477bec243e8b3702f04122a918154
FeRevoltes <fegutierres.fg@gmail.com>
2024-08-01 16:30:49 -0300

    operadores

---
+++ operadores.py
@@ -0,0 +1,31 @@
+"""
+Soma +
+Subtração -
+Multiplicação *
+Divisão /
+Divisão inteira //
+Resto da divisão %
+Potência **
+"""
+
+num1 = 50
+num2 = 5
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(20 % 3)
+print(2 ** 3)
+
+# Operadores booleanos
+idade1 = 20
+idade2 = 25
+
+print(idade1 > idade2)
+print(idade1 < idade2)
+print(idade1 <= idade2)
+print(idade1 >= idade2)
+print("Python" == "python")
+print("Python" != "python")
