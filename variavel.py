fd317f6ee3bae22550fc3919d41babfb851ecce7
FeRevoltes <fegutierres.fg@gmail.com>
2024-08-01 16:17:35 -0300

    Variaveis

---
+++ variavel.py
@@ -0,0 +1,18 @@
+# Tipos de variavel
+idade = 20 # Variavel do tipo int
+print(idade)
+
+altura = 1.81 # Variavel do tipo float
+print(altura)
+
+nome = "Fernando" # Variavel do tipo string
+print(nome)
+
+estudando = True # Variavel do tipo booleano
+print(estudando)
+
+print(type(estudando))
+
+# Obtendo dados
+linguagem = input("Qual linguagem de programação você está estudando? ")
+print("A linguagem que você está estudando é",linguagem)
