525488fc57078da61fea0627986b8a69ee86e099
FeRevoltes <fegutierres.fg@gmail.com>
2024-08-01 16:53:32 -0300

    conversão de tipo

---
+++ conversao.py
@@ -0,0 +1,14 @@
+num1 = "20"
+num2 = "30"
+print(num1 + num2)
+
+# converter
+numX = int(num1)
+print(numX + int(num2))
+#int()
+#str()
+#float()
+#bool()
+
+altura = float(input("Sua altura: "))
+print(altura, type(altura))
