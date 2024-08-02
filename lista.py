83c41686d616d7c404fc130245c2744e1adbade2
FeRevoltes <fegutierres.fg@gmail.com>
2024-08-01 23:01:26 -0300

    lista

---
+++ lista.py
@@ -0,0 +1,25 @@
+#Com liista 
+notas = [7.5, 10, 8.5]
+
+#Criando listas
+lista = []
+lista = list()
+lista = [22, "abacaxi", 14.4, False]
+lista_de_lista = [1, [1,2,3]]
+
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+print(lista[-1])
+print(lista[-2])
+print(lista[0:4])
+
+num = [10, 50, 30, 70, 20, 80, 5, 16]
+print(num[3:6])
+print(num[2:6:2])
+
+for i in num:
+    print(i)
+
+print(len(num))
